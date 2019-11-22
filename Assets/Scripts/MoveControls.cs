dd58aba986ff4acf000c5cce56a2ce556b38394a
Benny <benjamin.dums@gmail.com>
2019-11-22 19:57:37 +0100

    basic pacman animation and controls

---
+++ Assets/Scripts/MoveControls.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MoveControls : MonoBehaviour
+{
+    public GameObject player;
+    public float      moveSpeed = 1.0f;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        var direction = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            direction += Vector3.up;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            direction += Vector3.down;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            direction += Vector3.left;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            direction += Vector3.right;
+
+        var totalMovement = direction * moveSpeed * Time.fixedDeltaTime;
+        
+        Debug.Log("totalMovement: " + totalMovement);
+        
+        player.transform.position += totalMovement;
+    }
+}
