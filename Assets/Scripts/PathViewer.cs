43719a4c138ef0241614059eb9af631a41deb636
Tino777 <weber.t89@googlemail.com>
2019-11-23 12:53:12 +0100

    added PathViewer to GhostPrefab

--- Assets/Scripts/PathViewer.cs
+++ Assets/Scripts/PathViewer.cs
@@ -10,29 +10,30 @@ public class PathViewer : MonoBehaviour
 {
     public bool             shouldDraw = false;
     public bool shouldReset = false;
-    public List<Vector3Int> path       = new List<Vector3Int>();
+    public List<Vector3Int> path;
     public float            xOffset    = 0.08f;
     public float            yOffset    = 0.08f;
     public float            drawWidth  = 0.05f;
 
     private Grid         _grid;
     private LineRenderer _lineRenderer;
-
-    private bool isVanishing;
+    private Ghost _ghost;
 
     // Start is called before the first frame update
     void Start()
     {
         _grid = GameObject.Find("Grid").GetComponent<Grid>();
+        _ghost = GetComponent<Ghost>();
+
+        path = _ghost.movementPath;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (shouldDraw)
-        {
-            Draw();
-        }
+        path = _ghost.movementPath;
+        Draw();
+        
 
         if (shouldReset)
         {
@@ -59,30 +60,11 @@ public class PathViewer : MonoBehaviour
             pos.y += yOffset;
             _lineRenderer.SetPosition(i, pos);
         }
-
-        if (!isVanishing)
-        {
-            StartCoroutine(VanishPath(1));
-        }
     }
 
     public void Reset()
     {
         _lineRenderer.positionCount = 0;
         path.Clear();
-    }
-
-    private IEnumerator VanishPath(int seconds)
-    {
-        isVanishing = true;
-
-        while (path.Count > 0)
-        {
-            path.Remove(path.First());
-            Draw();
-            yield return new WaitForSeconds(seconds);
-        }
-
-        isVanishing = false;
     }
 }
